074ed1b6045e7b638728de456943680c4b5f9a12
Tom <tom.m.fisk@gmail.com>
2026-04-13 19:59:55 +0100

    Add coverage for when server is offline

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const client = new Client({
 const token = process.env.ClientToken;
 const serverID = '101752';
 
-async function GetPlayerCount() {
+async function GetServerData() {
   try {
     const response = await axios.get(`https://api.scplist.kr/api/servers/${serverID}`);
     return response.data;
@@ -22,8 +22,11 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   setInterval(async () => {
-    const data = await GetPlayerCount();
+    const data = await GetServerData();
       
+    if (!data.online)
+      return client.user.setActivity(`Server is offline`, ActivityType.Custom);
+
     if (!data || !data.players)
       return client.user.setActivity(`Player count unavailable`, ActivityType.Custom);
 
